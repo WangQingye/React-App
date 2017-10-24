cfbd2bee1101f688f8ce06503b2e8c13bba11b82
wangqingye <329103586@qq.com>
2017-10-24 14:49:34 +0800

    category

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { connect } from 'react-redux'
 import HomeHeader from '../../components/HomeHeader'
+import Category from '../../components/Category'
 
 class Home extends React.Component {
     constructor(props, context) {
@@ -12,6 +13,7 @@ class Home extends React.Component {
         return (
             <div>
                 <HomeHeader cityName={this.props.userinfo.cityName}/>
+                <Category/>
             </div>
         )
     }
