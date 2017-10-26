223113bf756d5eb58f3a7f28a2de5e3680295170
wangqingye <329103586@qq.com>
2017-10-26 15:22:22 +0800

    get list data

--- app/containers/Home/subpage/List.jsx
+++ app/containers/Home/subpage/List.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { getListData } from '../../../fetch/home/home'
-
 import ListCompoent from '../../../components/List'
 import LoadMore from '../../../components/LoadMore'
 
@@ -12,19 +11,40 @@ class List extends React.Component {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.state = {
+            data:[],
+            hasMore: false
         }
     }
     render() {
         return (
             <div>
-                {this.props.cityName}
+                <h2 className="home-list-title">猜你喜欢</h2>
+                <div>
+                    {this.state.data.toString()}
+                </div>
+
             </div>
         )
     }
     componentDidMount() {
-        console.log(this.props);
+        // 获取首页数据
+        this.loadFirstPageDate()
+    }
+    loadFirstPageDate(){
+        const cityName = this.props.cityName
+        const result = getListData(cityName, 0)
+        result.then(res => {
+            return res.json()
+        }).then(json => {
+            const hasMore = json.hasMore
+            const data = json.data
+            this.setState({
+                data: data,
+                hasMore: hasMore
+            })
+        })
     }
 }
-        
+
 
 export default List
